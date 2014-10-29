7da8afdb23d1acb768d4c9d9807d2e32f15baa6c
Kavan Bhavin <kab395@cornell.edu>
2014-10-28 23:39:54 -0400

    finished logical added inputs and outputs for shifter

--- logical.v
+++ logical.v
@@ -1,15 +1,17 @@
-module logical(); // add all inputs and outputs inside parentheses
+module logical(A, B, OP, Y);
+ 
 
   // inputs
-  
+  input [7:0] A;
+  input [7:0] B;
+  input OP;
   
   // outputs
-  
+  output [7:0] Y;
   
   // reg and internal variable definitions
   
-  
   // implement module here
-
+  assign Y = OP ? A & B : A | B;
   
 endmodule
--- shifter.v
+++ shifter.v
@@ -1,10 +1,13 @@
-module shifter(); // add all inputs and outputs inside parentheses
+module shifter(A, LA, LR, Y, C); // add all inputs and outputs inside parentheses
 
   // inputs
-  
+  input [7:0] A;
+  input LA;
+  input LR;
   
   // outputs
-  
+  output [7:0] Y;
+  output C;
   
   // reg and internal variable definitions
   
