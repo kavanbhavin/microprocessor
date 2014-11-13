3affd4a19e1c0f12708f797302eba65a1738b5f0
Kavan Bhavin <kab395@cornell.edu>
2014-11-12 21:37:54 -0500

    fixed possible bugs in halt_logic, still unverified

--- halt_logic.v
+++ halt_logic.v
@@ -6,19 +6,23 @@ module halt_logic(CLK, HALT, EN_L, H);
 	reg H;
 	reg prev_enable;
 	always@(posedge CLK)begin
-		if(~EN_L) begin
+		if(~HALT) begin
+			H <= 1'b0;
+			prev_enable <= EN_L;
+		end
+		else if(~EN_L) begin
 			if(prev_enable) begin
-				H <= 1'b1;
+				H <= 1'b0;
 				prev_enable <= EN_L;
 			end
 			else begin
-				H<= 1'b0;
+				H<= 1'b1;
 				prev_enable <= EN_L;
 			end
 		end
 		else begin
 			prev_enable <= EN_L;
-			H <= 1'b0;
+			H <= 1'b1;
 		end
 	end
 endmodule
